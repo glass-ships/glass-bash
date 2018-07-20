e524d0ec2a0ddd0296c9d867779d1df9d36946ca
glass-ships <glass.ships@outlook.com>
2018-07-19 23:06:42 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
+# add bashrc
 cp ./bashrc ~/.bashrc
+source ~/.bashrc
+
+# add ssh settings
 cp ./sshconfig ~/.ssh/config
 chmod 644 ~/.ssh/config 
 
+# set git name and email
 git config --global user.name "glass ships"
 git config --global user.email "glass.ships@outlook.com"
