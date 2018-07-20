31ed2c291d9658ea85b50789afc7a5c5801b8b7c
glass ships <glass.ships@outlook.com>
2018-07-19 22:09:00 -0400

    add init.sh

---
+++ init.sh
@@ -0,0 +1,6 @@
+cp ./bashrc ~/.bashrc
+cp ./sshconfig ~/.ssh/config
+chmod 644 ~/.ssh/config 
+
+git config --global user.name "glass ships"
+git config --global user.email "glass.ships@outlook.com"
