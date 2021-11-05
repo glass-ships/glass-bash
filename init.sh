5c1237c3683729a38a823857964492b58f0bb0d3
glass <g.elsarboukh@gmail.com>
2021-11-05 13:59:54 -0700

    add some default git ignore stuff

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ grep -qxF 'source '$DIR'/main' $HOME/.bashrc || echo 'source '$DIR'/main' >> $HO
 # VIM settings
 cp ./vimrc $HOME/.vimrc
 
-# make sure an SSH key exists
+# make sure an SSH key exists, copy config
 if [ ! -e $HOME/.ssh ]; then
     ssh-keygen -t rsa
     cp ./ssh-config $HOME/.ssh/config
@@ -22,3 +22,9 @@ fi
 # set git name and email
 git config --global user.name "glass"
 git config --global user.email "glass.ships@outlook.com"
+
+# set git to globally ignore ipynb_checkpoints
+git config --global core.excludesfile '~/.gitignore'
+echo '.ipynb_checkpoints' >> ~/.gitignore
+echo '*/.ipynb_checkpoints' >> ~/.gitignore
+
