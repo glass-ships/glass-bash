28a49ac4b2ce8767af8058ebca26ae7fcc281d8c
glass-ships <glass.ships@outlook.com>
2024-08-06 14:42:32 -0400

    add auto conda on cd

---
+++ scripts/conda-on-cd.sh
@@ -0,0 +1,13 @@
+if [[ -n "$CONDA_SHLVL" ]]; then
+    export CONDACONFIGDIR=""
+    cd() { builtin cd "$@" && 
+    if [ -f $PWD/.condaconfig ]; then
+        export CONDACONFIGDIR=$PWD
+        conda activate $(cat .condaconfig)
+    elif [ "$CONDACONFIGDIR" ]; then
+        if [[ $PWD != *"$CONDACONFIGDIR"* ]]; then
+            export CONDACONFIGDIR=""
+            conda deactivate
+        fi
+    fi }
+fi
