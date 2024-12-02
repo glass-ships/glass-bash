a0680ed30daab523346ac98c149ab49c92be5c4e
glass-ships <glass.ships@outlook.com>
2024-12-02 10:25:24 -0500

    add db backup aliases

--- scripts/ornl.sh
+++ scripts/ornl.sh
@@ -5,3 +5,7 @@ alias cdmantid='cd ~/dev/ornl/mantid'
 alias cdref='cd ~/dev/ornl/refl1d'
 alias cdsans='cd ~/dev/ornl/sans-backend'
 alias cdwebref='cd ~/dev/ornl/web_reflectivity'
+
+# livedata-prod aliases
+alias backup-db='nohup sudo docker exec -u postgres prod-db-1 pg_dump -Fc livedata > /backup/livedata_db-$(date -d "today" +"%Y%m%d_%H%M").dump > pg_dump-log.txt 2>pg_dump-log-errs.txt &'
+alias backup-status='ps aux | grep "pg_dump"'
