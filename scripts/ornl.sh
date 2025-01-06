bc747ae6a45832da2dc903ce904aed4737e2931e
glass-ships <glass.ships@outlook.com>
2025-01-06 10:23:48 -0500

    fix backup-db alias

--- scripts/ornl.sh
+++ scripts/ornl.sh
@@ -8,9 +8,8 @@ alias cdwebref='cd ~/dev/ornl/web_reflectivity'
 
 # LIVEDATA DB BACKUP
 
-# CMD TEMPLATE: docker exec -u <your_postgres_user> <postgres_container_name> pg_dump -Fc <database_name_here> > db.dump
-# TEST DB:      docker exec -u postgres test-db-1 pg_dump -Fc livedata > db.dump
+# CMD TEMPLATE: docker exec -u <your_postgres_user> <postgres_container_name> pg_dump <database_name_here> > db.dump
+# TEST DB:      docker exec -u postgres test-db-1 pg_dump livedata > db.dump
 # PROD DB:      sudo docker exec -u postgres prod-db-1 pg_dump -Fc livedata > /backup/livedata_db-$(date -d "today" +"%Y%m%d_%H%M").dump
-alias backup-db='nohup sudo docker exec -u postgres prod-db-1 pg_dump -Fc livedata > /backup/livedata_db-$(date -d "today" +"%Y%m%d_%H%M").dump 2>pg_dump-log-errs.txt &'
-# alias backup-db='nohup sudo docker exec -u postgres prod-db-1 pg_dump -Fc livedata > /backup/livedata_db-$(date -d "today" +"%Y%m%d_%H%M").dump > pg_dump-log.txt 2>pg_dump-log-errs.txt &'
+alias backup-db='nohup sudo docker exec -u postgres prod-db-1 pg_dump --verbose livedata > /backup/livedata_db-$(date -d "today" +"%Y%m%d_%H%M").dump 2>pg_dump-log-errs.txt &'
 alias backup-status='ps aux | grep "pg_dump"'
