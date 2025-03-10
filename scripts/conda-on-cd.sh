e8b1f2e34c73d5a76c48a9850bdf223f07b61d24
glass-ships <glass.ships@outlook.com>
2025-03-09 20:19:48 -0400

    tweak conda-on-cd.sh

--- scripts/conda-on-cd.sh
+++ scripts/conda-on-cd.sh
@@ -1,13 +1,33 @@
-if [[ -n "$CONDA_SHLVL" ]]; then
-    export CONDACONFIGDIR=""
-    cd() { builtin cd "$@" &&
-        if [ -f $PWD/.condaconfig ]; then
-            export CONDACONFIGDIR=$PWD
+# if [[ -n "$CONDA_SHLVL" ]]; then
+#     export CONDACONFIGDIR=""
+#     cd() { builtin cd "$@" &&
+#         if [ -f $PWD/.condaconfig ]; then
+#             export CONDACONFIGDIR=$PWD
+#             conda activate $(cat .condaconfig)
+#         elif [ "$CONDACONFIGDIR" ]; then
+#             if [[ $PWD != *"$CONDACONFIGDIR"* ]]; then
+#                 export CONDACONFIGDIR=""
+#                 conda deactivate
+#             fi
+#         fi; }
+# fi
+
+cd() {
+    builtin cd "$@" &&
+        if [ -f $PWD/.condaconfig ] && [ -n "$CONDA_SHLVL" ]; then
             conda activate $(cat .condaconfig)
-        elif [ "$CONDACONFIGDIR" ]; then
-            if [[ $PWD != *"$CONDACONFIGDIR"* ]]; then
-                export CONDACONFIGDIR=""
-                conda deactivate
+            export VENVDIR=$PWD
+            export ISCONDAENV=1
+        elif [ -f $PWD/.venv/Scripts/activate ]; then
+            . .venv/Scripts/activate
+            export VENVDIR=$PWD
+        elif [ -f $PWD/.venv/bin/activate ]; then
+            . .venv/bin/activate
+            export VENVDIR=$PWD
+        elif [ "$VENVDIR" ]; then
+            if [[ $PWD != *"$VENVDIR"* ]]; then
+                conda deactivate || deactivate
+                export VENVDIR=""
             fi
-        fi; }
-fi
+        fi
+}
