27bbcdcea0989f8ea9cba61b2262e9383535f696
Glass <glass.ships@protonmail.com>
2026-02-25 12:27:50 -0500

    fix update-cloudflared

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -46,7 +46,6 @@ function update-cloudflared() {
 		wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 		chmod +x cloudflared-linux-amd64
 		sudo mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
-	fi
 	else
 		echo "Unsupported distribution - please install cloudflared manually"
 		return 1
